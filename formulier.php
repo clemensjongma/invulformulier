91b85b8ae0ad02d189aad195e9896779aa2f8e4f
ClemensJongma <clemensjongma@gmail.com>
2019-01-04 13:10:53 +0100

    werkende berekeningen

--- formulier.php
+++ formulier.php
@@ -3,25 +3,29 @@
 <h1>Rekenvel Zwembad </h1>
 
 <form action="uitkomst.php" method="post">
-De lengte van uw zwembad: 
+Gewenste lengte in meters: 
 <br>
 <input type="text" name="lengte" value="<?php echo $lengte;?>">  
 <br>
-De breedte: 
-<br>
+Gewenste breedte in meters: <br>
 <input type="text" name="breedte" value="<?php echo $breedte;?>">
 <br>
-De diepte: 
-<br>
+Gewenste diepte in meters: <br>
 <input type="text" name="diepte" value="<?php echo $diepte;?>">
 <br>
+Gewenste rand in meters: <br>
+<input type="text" name="rand" value="<?php echo $rand;?>">
+<br>
+Gewenst verschil tussen diepte en waterniveau:  <br>
+<input type="text" name="marge" value="<?php echo $marge;?>">
+<br>
+Gewenste bekleding: <br>
+<input type="radio" name="bekleding" value="Geschilderd"> Geschilderd<br>
+<input type="radio" name="bekleding" value="Betegeld"> Betegeld<br>
 
-
-
-
-
-<input type="submit" name="submit" value="Submit">  
+<input type="submit" name="submit" value="Vraag offerte aan">  
 </form> 
+
 
  
 
--- uitkomst.php
+++ uitkomst.php
@@ -2,34 +2,58 @@
 
 
 <?php
-
+//benodigde functies
     function oppervlakte($getal1,$getal2) {
-        global $e;
-        $e = $getal1*$getal2;
+        $opp = 0;
+        $opp = $getal1*$getal2;
+        return $opp;
     }
     
     function som($getal3,$getal4)
-    {global $som;
+    {   $som = 0;
         $som =  $getal3 + $getal4;
+        return $som;
     }
     
     function inhoud($lengte,$breedte,$diepte){
-        global $inhoud;
-            $inhoud = $lengte * $breedte * $diepte;
+        $inhoud = 0;
+        $inhoud = $lengte * $breedte * $diepte;
+        return $inhoud;
     }
 
+    function omtrek($lengte,$breedte){
+        $omtrek = 0;    
+        $omtrek = 2 * ($lengte + $breedte);
+        return $omtrek;
+    }
+//van invulformulier   
 $lengte = $_POST["lengte"];
 $breedte = $_POST["breedte"];
 $diepte = $_POST["diepte"];
-// oppervlakte($x,$y);
-// som($x,$y);
-inhoud($lengte,$breedte,$diepte);
 
+//berekeningen
+$tempInhoud = inhoud($lengte,$breedte,$diepte);
+$tempOppervlakte = oppervlakte($lengte,$breedte);
+$tempOmtrek = omtrek($lengte,$breedte);
+//totale wandoppervlakte = omtrek * diepte
+$tempWandOppervlakte = oppervlakte($tempOmtrek,$diepte);
 ?>
 <br/>
 <?php
+echo "Uw invoer: lengte ".$lengte." m. <br>";
+echo "Uw invoer: breedte ".$breedte." m. <br>";
+echo "Uw invoer: diepte ".$diepte." m. <br>";
 
-echo "De inhoud van uw zwembad is ".$inhoud." m3.";
+echo "Af te graven grond: ".$tempInhoud." m3.";
+echo "<br>";
+echo "Totaaloppervlakte wanden: ".$tempWandOppervlakte." m2.";
+echo "<br>";
+echo "Oppervlakte vloer: ".$tempOppervlakte." m2.";
+echo "<br>";
+echo "Oppervlakte afdekzeil: ".$tempOppervlakte." m2.";
+echo "<br>";
+echo "Randafwerking afdekzeil: ".$tempOmtrek." m.";
+
 
 // echo $_POST["first"]; 
 // echo $_POST["second"];
